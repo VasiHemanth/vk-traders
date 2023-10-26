cb0e50d8bb7f804fb0b7b1cb19dc7df7a8906f25
Hemanth Vasi <hemanth.vasi1716@gmail.com>
2023-10-26 15:00:32 +0530

    EnvAPI changed from lgr logistics to vk traders

--- app/lib/EnvAPI.js
+++ app/lib/EnvAPI.js
@@ -1,7 +1,7 @@
 export default function EnvAPI() {
   let apiUrl = "";
   if (process.env.NODE_ENV === "production") {
-    apiUrl = "https://lgr-logistics.vercel.app";
+    apiUrl = "https://vk-traders.vercel.app";
   } else if (process.env.NODE_ENV === "development") {
     apiUrl = "http://localhost:3000";
   }
